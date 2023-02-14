80411ebe7b34c2196a222e106bbb6e587eecff94
0x3c <0x3ceth@gmail.com>
2023-02-14 23:01:04 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,51 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.css';
-import mainImage from '@/assets/images/main_image.jpeg';
-import { Form, Button, Spinner } from 'react-bootstrap';
-import { FormEvent } from 'react';
-import { useState } from 'react';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import mainImage from '@/assets/images/main_image.jpg'
+import { Form, Button, Spinner } from 'react-bootstrap'
+import { FormEvent, useState } from 'react'
 
 export default function Home() {
-  const [quote, setQuote] = useState('');
+
+  const [quote, setQuote] = useState("");
   const [quoteLoading, setQuoteLoading] = useState(false);
   const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const prompt = formData.get('prompt')?.toString().trim();
+    const prompt = formData.get("prompt")?.toString().trim();
+
     if (prompt) {
       try {
-        setQuote('');
+        setQuote("");
         setQuoteLoadingError(false);
         setQuoteLoading(true);
-        const response = await fetch(
-          '/api/cringe?prompt=' + encodeURIComponent(prompt)
-        );
+
+        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
         const body = await response.json();
         setQuote(body.quote);
-      } catch (e) {
-        console.error(e);
+      } catch (error) {
+        console.error(error);
         setQuoteLoadingError(true);
       } finally {
         setQuoteLoading(false);
       }
     }
   }
+
   return (
     <>
       <Head>
         <title>Cringe AI - Create cringy motivational quotes</title>
-        <meta name='description' content='by Coding in Flow' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1>Cringe AI</h1>
         <h2>powered by GPT-3</h2>
-        <div>
-          Enter a topic and the AI will generate a super cringy motivational
-          quotes
-        </div>
+        <div>Enter a topic and the AI will generate a super cringy motivational quote</div>
         <div className={styles.mainImageContainer}>
           <Image
             src={mainImage}
@@ -71,9 +69,9 @@ export default function Home() {
           </Button>
         </Form>
         {quoteLoading && <Spinner animation='border' />}
-        {quoteLoadingError && 'Something went wrong. Please try again.'}
+        {quoteLoadingError && "Something went wrong. Please try again."}
         {quote && <h5>{quote}</h5>}
       </main>
     </>
-  );
+  )
 }
