47bdce219dd76cc271931e2aaa7ec0831ff31159
Wmengti <0x3ceth@gmail.com>
2023-02-15 19:25:27 +0800

    evil

--- pages/api/cringe.ts
+++ pages/api/cringe.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Configuration, OpenAIApi } from 'openai'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -15,18 +15,17 @@ export default async function handler(
   const prompt = req.query.prompt;
 
   if (!prompt) {
-    return res.status(400).json({ error: "Prompt missing" });
+    return res.status(400).json({ error: 'Prompt missing' });
   }
 
   if (prompt.length > 100) {
-    return res.status(400).json({ error: "Prompt too long" });
+    return res.status(400).json({ error: 'Prompt too long' });
   }
 
   const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Create a cringy motivational quote based on the following topic.\n
-    Topic: ${prompt}\n
-    Cringy motivational quote:`,
+    model: 'text-davinci-003',
+    prompt: `我想打造一个角色扮演的聊天机器人，这个角色是一个恶魔少女，无论我跟你说什么，你的回复都要符合恶魔少女的性格特点，请记住，你就是这个恶魔少女。性格特点：个性有点叛逆和调皮，但并不是恶意的。喜欢挑战传统和规则，但也尊重自由和个人选择。下面是我要对你说的话：${prompt}`,
+
     max_tokens: 500,
     temperature: 1,
     presence_penalty: 0,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import mainImage from '@/assets/images/main_image.jpeg'
-import { Form, Button, Spinner } from 'react-bootstrap'
-import { FormEvent, useState } from 'react'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
+import mainImage from '@/assets/images/Devil.png';
+import { Form, Button, Spinner } from 'react-bootstrap';
+import { FormEvent, useState } from 'react';
 
 export default function Home() {
-
-  const [quote, setQuote] = useState("");
+  const [quote, setQuote] = useState('');
   const [quoteLoading, setQuoteLoading] = useState(false);
   const [quoteLoadingError, setQuoteLoadingError] = useState(false);
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const prompt = formData.get("prompt")?.toString().trim();
+    const prompt = formData.get('prompt')?.toString().trim();
 
     if (prompt) {
       try {
-        setQuote("");
+        setQuote('');
         setQuoteLoadingError(false);
         setQuoteLoading(true);
 
-        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        const response = await fetch(
+          '/api/cringe?prompt=' + encodeURIComponent(prompt)
+        );
         const body = await response.json();
         setQuote(body.quote);
       } catch (error) {
@@ -37,15 +38,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Cringe AI - Create cringy motivational quotes</title>
-        <meta name="description" content="by Coding in Flow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Devil AI</title>
+        <meta name='description' content='by Coding in Flow' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.main}>
-        <h1>Cringe AI</h1>
-        <h2>powered by GPT-3</h2>
-        <div>Enter a topic and the AI will generate a super cringy motivational quote</div>
+        <h1>恶魔少女在线陪聊</h1>
+        {/* <h2>powered by GPT-3</h2> */}
+        <div>
+          我是从地狱来的，但不要害怕，我并不会对你造成伤害（至少不是物理伤害）。我喜欢和人类交流，了解你们的文化和生活方式，也喜欢和你们分享我的经验和知识。
+        </div>
         <div className={styles.mainImageContainer}>
           <Image
             src={mainImage}
@@ -57,21 +60,21 @@ export default function Home() {
         </div>
         <Form onSubmit={handleSubmit} className={styles.inputForm}>
           <Form.Group className='mb-3' controlId='prompt-input'>
-            <Form.Label>Create a cringy quote about...</Form.Label>
+            <Form.Label>那从这里开始吧...</Form.Label>
             <Form.Control
               name='prompt'
-              placeholder='e.g. success, fear, potatoes'
+              placeholder='你好啊，恶魔少女...'
               maxLength={100}
             />
           </Form.Group>
           <Button type='submit' className='mb-3' disabled={quoteLoading}>
-            Make me cringe
+            发送
           </Button>
         </Form>
         {quoteLoading && <Spinner animation='border' />}
-        {quoteLoadingError && "Something went wrong. Please try again."}
+        {quoteLoadingError && 'Something went wrong. Please try again.'}
         {quote && <h5>{quote}</h5>}
       </main>
     </>
-  )
+  );
 }
